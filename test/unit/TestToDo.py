c1d7421c89037702b0bf8e617a199c0e4830893b
JosephAndVit <63151673+JosephAndVit@users.noreply.github.com>
2023-06-11 14:51:41 +0200

    Quito test_list_todo_error

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -128,17 +128,6 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertTrue(result[0]['text'] == self.text)
         print ('End: test_list_todo')
 
-    def test_list_todo_error(self):
-        print ('---------------------')
-        print ('Start: test_list_todo_error')
-        # Testing file functions
-        from src.todoList import put_item
-        from src.todoList import get_item        
-        # Table mock
-        self.assertRaises(Exception, put_item("", self.dynamodb))
-        self.assertRaises(Exception, put_item("", self.dynamodb))
-        print ('End: test_list_todo_error')
-
     def test_update_todo(self):
         print ('---------------------')
         print ('Start: test_update_todo')
